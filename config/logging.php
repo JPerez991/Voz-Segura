a6338f12bb1a0cfade512d82e0fdfb5198cfc055
juan <pjuanpa37@gmail.com>
2026-06-15 13:35:41 -0500

    Fix: default log to stderr on Vercel

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', env('VERCEL_ENV') ? 'stderr' : 'stack'),
 
     /*
     |--------------------------------------------------------------------------
